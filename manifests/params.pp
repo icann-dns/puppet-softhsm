4d528c59e242a86b26b933ab50dbab1768c33a9c
Bas Toonk <btoonk@bol.com>
2018-06-21 16:33:56 +0200

    Add redhat defaults

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,28 @@
 #== Class: softhsm
 #
 class softhsm::params {
-  case $::lsbdistcodename {
-    'trusty': {
-      $version   = 1
+  case $::operatingsystem {
+    'ubuntu': {
+      case $::lsbdistcodename {
+        'trusty': {
+          $version   = 1
+          $package   = 'softhsm'
+          $utils_cmd = 'softhsm'
+          $conf_file = '/etc/softhsm/softhsm.conf'
+        }
+        default: {
+          $version   = 2
+          $package   = 'softhsm2'
+          $utils_cmd = 'softhsm2-util'
+          $conf_file = '/etc/softhsm/softhsm2.conf'
+        }
+      }
+    }
+    'RedHat': {
+      $version   = 2
       $package   = 'softhsm'
-      $utils_cmd = 'softhsm'
-      $conf_file = '/etc/softhsm/softhsm.conf'
+      $utils_cmd = 'softhsm2-util'
+      $conf_file = '/etc/softhsm2.conf'
     }
     default: {
       $version   = 2
